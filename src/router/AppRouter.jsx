c71ab8bb95a0caf279f661708518a3882b51c3a0
JaacielBriseo <jaacielbris23@gmail.com>
2022-10-07 16:20:44 -0700

    Paths y clases de navbar añadidos

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,17 +1,18 @@
-import { Navigate, Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from 'react-router-dom';
+import { MarvelPage, DCpage } from '../heroes';
+import { LoginPage } from '../auth';
+import { Navbar } from '../ui';
 
-import {LoginPage} from '../auth/pages/LoginPage'
-import {DCpage} from '../heroes/pages/DCpage'
-import {MarvelPage} from '../heroes/pages/MarvelPage'
 export const AppRouter = () => {
   return (
     <>
-    <Routes>
-        <Route path="marvel" element={<MarvelPage/>}/>
-        <Route path="dc" element={<DCpage/>}/>
-        <Route path="login" element={<LoginPage/>}/>
-        <Route path="/" element={<Navigate to = '/marvel'/>}/>
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="marvel" element={<MarvelPage />} />
+        <Route path="dc" element={<DCpage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="/" element={<Navigate to="/marvel" />} />
+      </Routes>
     </>
-  )
-}
+  );
+};
