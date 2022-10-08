4e22b693a1289c616611c0ac9bb4a0e2662e4af8
JaacielBriseo <jaacielbris23@gmail.com>
2022-10-08 00:30:50 -0700

    Cards de superheroes agregados

--- src/heroes/components/HeroList.jsx
+++ src/heroes/components/HeroList.jsx
@@ -1,4 +1,5 @@
 import { getHeroesByPublisher } from '../helpers';
+import { HeroCard } from './HeroCard';
 
 export const HeroList = ({ publisher }) => {
   const heroes = getHeroesByPublisher(publisher);
@@ -9,10 +10,11 @@ export const HeroList = ({ publisher }) => {
 
   return (
     /* {listItems} */
-    <ul>
+    <div className='row rows-cols-1 row-cols-md-3 g-3'>
       {heroes.map((heroe) => (
-        <li key={heroe.id}>{heroe.superhero}</li>
+      <HeroCard key={heroe.id}
+      {...heroe}/>
       ))}
-    </ul>
+    </div>
   );
 };
