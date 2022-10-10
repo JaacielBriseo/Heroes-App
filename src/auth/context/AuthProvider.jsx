8d6c08ebc5cfa21b4eb8a31411482303d33c1adf
JaacielBriseo <jaacielbris23@gmail.com>
2022-10-09 20:18:22 -0700

    Login de usuario añadido

--- src/auth/context/AuthProvider.jsx
+++ src/auth/context/AuthProvider.jsx
@@ -1,14 +1,27 @@
 import { useReducer } from 'react';
+import { types } from '../types/types';
 import { AuthContext } from './AuthContext';
 import { authReducer } from './authReducer';
 const initialState = {
   logged: false,
 };
 export const AuthProvider = ({ children }) => {
-  const [state, dispatch] = useReducer(authReducer, initialState);
+  const [authState, dispatch] = useReducer(authReducer, initialState);
 
+  const login =  (name = '') => {
+    const action = {
+      type: types.login,
+      payload: {
+        id: 'ABC',
+        name: name
+      }
+    }
+    dispatch(action)
+  }
+
+  
   return (
-  <AuthContext.Provider value={{}}>
+  <AuthContext.Provider value={{login : login, ...authState}}>
     {children}
   </AuthContext.Provider>
   )
--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,19 +1,26 @@
-import { useNavigate } from "react-router-dom";
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 export const LoginPage = () => {
+
+  const { login } = useContext(AuthContext);
   const navigate = useNavigate();
 
   const onLogin = () => {
+    login('Jaaciel Briseño')
     navigate('/', {
-        replace : true
+      replace: true,
     });
   };
   return (
-    <div className="container-mt5">
+    <div className="container mt-5">
       <h1>Login</h1>
       <hr />
 
-      <button className="btn btn-primary" onClick={onLogin}>Login</button>
+      <button className="btn btn-primary" onClick={onLogin}>
+        Login
+      </button>
     </div>
   );
 };
