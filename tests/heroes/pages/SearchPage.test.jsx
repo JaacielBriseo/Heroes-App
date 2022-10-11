8380d8a22c45e5531a5c2d474f46befb298b7de1
JaacielBriseo <jaacielbris23@gmail.com>
2022-10-11 15:35:11 -0700

    Pruebas en componente SearchPage del inputValue y useNavigate()

--- tests/heroes/pages/SearchPage.test.jsx
+++ tests/heroes/pages/SearchPage.test.jsx
@@ -1,8 +1,15 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { SearchPage } from '../../../src/heroes/pages/SearchPage';
 
+const mockedUseNavigate = jest.fn();
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useNavigate: () => mockedUseNavigate,
+}));
+
 describe('Pruebas en <SearchPage/>', () => {
+  beforeEach(() => jest.clearAllMocks());
   test('debe de mostrarse correctamente con valores por defecto', () => {
     const { container } = render(
       <MemoryRouter>
@@ -23,5 +30,32 @@ describe('Pruebas en <SearchPage/>', () => {
 
     const img = screen.getByRole('img');
     expect(img.src).toContain('/assets/heroes/dc-batman.jpg');
+  });
+  test('debe de mostrar un error si no se encuentra el heroe', () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={['/search?q=batman123']}>
+        <SearchPage />
+      </MemoryRouter>
+    );
+    const input = screen.getByRole('textbox');
+    expect(input.value).toBe('batman123');
+    // screen.debug();
+    expect(screen.getByText('No hero with')).toBeTruthy();
+  });
+  test('debe de llamar el navigate a la pantalla nueva', () => {
+    render(
+      <MemoryRouter initialEntries={['/search']}>
+        <SearchPage />
+      </MemoryRouter>
+    );
+    const input = screen.getByRole('textbox');
+    fireEvent.change(input, {
+      target: { name: 'searchText', value: 'superman' },
+    });
+
+    const form = screen.getByRole('form');
+    fireEvent.submit(form);
+
+    expect(mockedUseNavigate).toHaveBeenCalledWith('?q=superman')
   });
 });
