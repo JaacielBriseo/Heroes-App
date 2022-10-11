54c23080320dea06abd34081c012ed2370ec4075
JaacielBriseo <jaacielbris23@gmail.com>
2022-10-11 15:12:32 -0700

    Pruebas en componente SearchPage con queryParams

--- tests/heroes/pages/SearchPage.test.jsx
+++ tests/heroes/pages/SearchPage.test.jsx
@@ -1,15 +1,27 @@
-import { render,screen } from '@testing-library/react'
-import { MemoryRouter } from 'react-router-dom'
-import { SearchPage } from '../../../src/heroes/pages/SearchPage'
+import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+import { SearchPage } from '../../../src/heroes/pages/SearchPage';
 
-describe('Pruebas en <SearchPage/>', () => { 
-    test('debe de mostrarse correctamente con valores por defecto', () => { 
-       const {container } = render(
-            <MemoryRouter>
-                <SearchPage/>
-            </MemoryRouter>
-        )
-        // screen.debug()
-        expect(container).toMatchSnapshot()
-     })
- })
+describe('Pruebas en <SearchPage/>', () => {
+  test('debe de mostrarse correctamente con valores por defecto', () => {
+    const { container } = render(
+      <MemoryRouter>
+        <SearchPage />
+      </MemoryRouter>
+    );
+    // screen.debug()
+    expect(container).toMatchSnapshot();
+  });
+  test('debe de mostrarse correctamente con valores por defecto', () => {
+    const { container } = render(
+      <MemoryRouter initialEntries={['/search?q=batman']}>
+        <SearchPage />
+      </MemoryRouter>
+    );
+    const input = screen.getByRole('textbox');
+    expect(input.value).toBe('batman');
+
+    const img = screen.getByRole('img');
+    expect(img.src).toContain('/assets/heroes/dc-batman.jpg');
+  });
+});
