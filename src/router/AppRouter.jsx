2e8a948ecd1524ea3df480d8319000336f47a7d1
JaacielBriseo <jaacielbris23@gmail.com>
2022-10-10 17:44:18 -0700

    Ruta publica añadida

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,12 +2,20 @@ import { Route, Routes } from 'react-router-dom';
 import { HeroesRoutes } from '../heroes';
 import { LoginPage } from '../auth';
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const AppRouter = () => {
   return (
     <>
       <Routes>
-        <Route path="login" element={<LoginPage />} />
+        <Route
+          path="login"
+          element={
+            <PublicRoute>
+              <LoginPage />
+            </PublicRoute>
+          }
+        />
 
         <Route
           path="/*"
@@ -17,6 +25,8 @@ export const AppRouter = () => {
             </PrivateRoute>
           }
         />
+
+        {/* <Route path="login" element={<LoginPage />} /> */}
         {/* <Route path="/*" element={<HeroesRoutes />} /> */}
       </Routes>
     </>
